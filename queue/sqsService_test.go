bac5034e33c0838399d49bdfd005a604afed3977
Joel Ogden <joel.a.ogden@gmail.com>
2023-12-01 16:18:44 +0000

    added mock and simple sqs tests

--- queue/sqsService_test.go
+++ queue/sqsService_test.go
@@ -1,1 +1,33 @@
 package queue
+
+import (
+	"context"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestPushToQueue(t *testing.T) {
+	ctx := context.Background()
+
+	mockSqsClient := &sqsClientMock{""}
+	service := SqsService{
+		Client: mockSqsClient,
+	}
+
+	msg := "this is a message"
+	given := Message{Body: msg}
+
+	err := service.PushToQueue(ctx, given)
+	assert.NoError(t, err)
+	assert.Equal(t, msg, mockSqsClient.messagePushed)
+
+}
+
+type sqsClientMock struct {
+	messagePushed string
+}
+
+func (s *sqsClientMock) Push(ctx context.Context, message string) error {
+	s.messagePushed = message
+	return nil
+}
